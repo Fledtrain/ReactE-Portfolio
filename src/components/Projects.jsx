df6e0ef7aa78defa07a78e1f845ff4f9202c9d45
Fledtrain <Joeyew2014@gmail.com>
2023-09-28 17:49:23 -0400

    Removed React import not needed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { FaGithub, FaLink } from "react-icons/fa";
 
 const Projects = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Nav from "../components/Nav";
 import Hero from "../components/Hero";
 import Tech from "../components/Tech";
